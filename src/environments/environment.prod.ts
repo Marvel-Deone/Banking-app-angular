ef951c2929d5c8c83d3b2df368ea09f4a7759457
Marvel-Deone <89777325+Marvel-Deone@users.noreply.github.com>
2023-02-21 16:16:16 +0100

    Added a new update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  Url: 'https://bankapi.vercel.app/api',
+  baseUrl: 'https://bankapi.vercel.app/api',
+  documentLink: 'https://bankapi.vercel.app/api'
 };
